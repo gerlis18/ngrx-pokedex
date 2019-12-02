53dd3664f92fbec8a462153883f814008a88376c
galvarez <galvarez@intergrupo.com>
2019-12-02 08:58:59 -0500

    Ajustando detalles.

--- src/app/components/edit-profile/edit-profile.component.ts
+++ src/app/components/edit-profile/edit-profile.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { LocalstorageService } from '../../services/localstorage.service';
 import { UserModel } from '../../models/user.model';
 import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../reducers';
+import { updatePokemonList } from '../../actions/pokemon.actions';
 
 @Component({
   selector: 'app-edit-profile',
@@ -16,7 +19,10 @@ export class EditProfileComponent implements OnInit {
     email: ''
   };
 
-  constructor(private localService: LocalstorageService, private router: Router) { }
+  constructor(
+    private localService: LocalstorageService,
+    private store: Store<AppState>,
+    private router: Router) { }
 
   ngOnInit() {
     this.user = this.localService.getUser();
@@ -24,6 +30,7 @@ export class EditProfileComponent implements OnInit {
 
   save() {
     this.localService.setUser(this.user);
+    this.store.dispatch(updatePokemonList({ indexScroll: (0) }));
     this.router.navigate(['/home']);
   }
 
